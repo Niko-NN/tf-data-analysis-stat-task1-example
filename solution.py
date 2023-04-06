33a4a2c24a349b84c2e6bdba2beb43fa782c1791
Nick <117512008+Niko-NN@users.noreply.github.com>
2023-04-06 16:03:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import numpy as np
 chat_id = 5459656416 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = len(x)
-    error = np.random.uniform(-1, np.exp(1), n)
-    v = x + error
-    acceleration = 2 * np.sum(v) / (n * 10)
+    errors = np.random.uniform(-1, np.exp(1), size=len(x))
+    x += errors
+    time = 10
+    acceleration = 2 * (np.mean(x) / time)**2
     return acceleration
